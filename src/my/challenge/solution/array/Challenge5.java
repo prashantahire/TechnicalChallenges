e808d5572f4f94129d36b31a8f4abb35b3a6db46
Prashant Ahire <33499614+prashantahire@users.noreply.github.com>
2019-05-24 12:14:21 -0700

    Update Challenge5.java

--- src/my/challenge/solution/array/Challenge5.java
+++ src/my/challenge/solution/array/Challenge5.java
@@ -6,7 +6,6 @@ package my.challenge.solution.array;
 public class Challenge5 {
     public static void main(String[] args) {
         int[] a = { 0, 0, 4, 0, 0, 8, 0, 0, 9, 0, 10 };
-        
         moveZeros(a);
          moveZerosToEnd(a);
          int[] b = { 1, 0, 4, 0, 0, 8, 0, 0, 9, 0, 10 };
@@ -30,7 +29,6 @@ public class Challenge5 {
         for (int e : a)
             System.out.print(" " + e);
     }
-
     public static void moveZeros(int a[]) {
         int j = a.length - 1;
         int i = 0;
@@ -49,7 +47,6 @@ public class Challenge5 {
         for (int e : a)
             System.out.print(" " + e);
     }
-    
     public static void moveZerosMaintainOrder(int a[]) {
         int count = 0;
         for(int i=0; i<a.length; i++)
